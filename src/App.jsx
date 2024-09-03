e3bee9ff1eb86a2de830058324b0feb5997326a8
AsadAli7789 <155202559+AsadAli7789@users.noreply.github.com>
2024-09-03 13:26:39 -0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,11 @@ let filteredArr = product.filter((data) => {
         Products
       </h1>
       <div className="w-3/4 mx-auto">
+      
         <input
           placeholder="Search"
           type="search"
-          className="w-full border-2  p-3 font-bold"
+          className="w-full border-2  p-3 font-bold  text-black"
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
@@ -41,7 +42,7 @@ let filteredArr = product.filter((data) => {
         <input
           placeholder="Category"
           type="category"
-          className="w-full border-2  p-3 font-bold"
+          className="w-full border-2  p-3 font-bold  text-black"
           onChange={(e) => setCategory(e.target.value)}
         />
       </div>
@@ -49,36 +50,71 @@ let filteredArr = product.filter((data) => {
         <input
           placeholder="Price"
           type="number"
-          className="w-full border-2  p-3 font-bold"
+          className="w-full border-2  p-3 font-bold  text-black"
           onChange={(e) => setPrice(e.target.value)}
         />
       </div>
-      <div className="flex flex-wrap m-4">
-        {filteredArr.map((data) => (
-          <a
-          key={data.id}
-          href="#"
-          className="flex flex-col items-center m-10 mz-auto bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-        >
-          <img
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-            src={data.image}
-            alt=""
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {data.title}
-            </h5>
-            <h5>{data.category}</h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {data.description}
-            </p>
-            <h4>{data.price}</h4>
-          </div>
-        </a>
-        
-        ))}
+      
+
+
+
+
+
+<div className="flex flex-wrap justify-center	">
+  {
+    filteredArr.map((data)=>(
+      <div>
+        <div class="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mx-3 my-3 jus">
+  <div class="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+    src={data.image}
+    alt={data.title}
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      {data.title}
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      {data.price}
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    {data.description}
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
       </div>
+    ))
+  }
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
